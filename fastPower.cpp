41027d4f70a004a64578aba1ec88cc39c5d87586
Iceki <72178099+lceki@users.noreply.github.com>
2021-10-22 15:14:29 +0800

    Add files via upload

---
+++ fastPower.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+const ll mod = 1e7;
+
+//a ^ b
+ll fastpower(ll a,ll b,ll m=mod)
+{
+    if (b == 0)return 1;
+    ll y = 1, x = a%m;
+    while (b>1)
+    {
+        
+            if (b % 2 == 1)
+                y = (y * x)%m;
+
+                x =(x*x)%m;
+            
+        b >>= 1;
+    }
+    y = (y * x)%m;
+    return y;
+}
+
+
+
+
+int main() {
+    cout  << fastpower(5, 7459,31847) << endl;
+    cout << fastpower(2,11, 31847) << endl;
+    return 0;
+}
