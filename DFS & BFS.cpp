66206756bed6fcfaf775f71873804ac094aeda6c
Iceki <72178099+lceki@users.noreply.github.com>
2022-05-19 23:56:48 +0800

    Add files via upload

---
+++ DFS & BFS.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}/*
+void BFS()
+{
+
+    while(!queue.empty())
+    {
+    now=queue.front();
+
+    //操作now节点
+    
+    while(son_exist&&!mark)
+        {
+         queue.push();
+         mark();    //这个mark是在入队的时候就要加上
+
+        }
+    queue.pop();
+    
+    }
+
+
+}
+
+void DFS()  //非递归版
+{
+    stack.push(0);
+
+    while(!stack.empty())
+    {
+        now=stack.top();
+        if(!marked now)
+        {
+                //操作now节点
+                mark(now);
+
+        }
+
+        while(neighbor)
+         {
+         if(!mark)
+            {
+            stack.push();
+            found=true;
+            break;
+            }
+         }
+
+         if(!found)
+         stack.pop();
+
+    }
+}
+void DFS(now)  //递归版
+{
+    //操作now节点
+    mark(now);
+    
+    for(neighbor)
+    {
+    if(!mark)
+    DFS();
+    }
+
+}
