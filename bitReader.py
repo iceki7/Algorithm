76b95214f292dec20c2e234218869e98e69c049d
iceki7 <1300953421@qq.com>
2023-10-26 16:10:27 +0800

    bit reader

---
+++ bitReader.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 15:59:41 2023
+
+@author: 34915
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 15:59:25 2023
+
+@author: 34915
+"""
+
+
+def test(file):
+    with open(file, "rb") as file:
+        byte = file.read(1)
+        while byte:
+            # 处理每个字节
+            print(byte,end=' ')
+            byte = file.read(1)
+test("source.txt")
+print('\n\n')
+test("source.zip")
+print('\n\n')
+test("source-enc.zip")
---
+++ syntax.cpp
@@ -0,0 +1,26 @@
+#include "gnn_damage/gnn_damage_solver.hpp" //工程路径
+#include <iostream>
+
+#ifndef GNN
+#define Pi 123
+#endif
+
+nullptr
+malloc
+memset
+memcpy
+auto
+
+
+
+//.h .hpp
+using namespace std;
+std::cout<<"F:\\yzx\\Physika\\pinnmodel\\m-1697103207";
+namespace nm{
+
+    class c :public b{
+
+    };
+
+
+}
