96b2790049f309719add871105a4f8114b7a0c88
iceki7 <1300953421@qq.com>
2023-11-09 23:05:55 +0800

    DFT add high-frequency sample test

--- DFT2.py
+++ DFT2.py
@@ -18,17 +18,18 @@ signum=50 #sample num
 
 freqmin=1/siglen
 
+np.random.seed(1234)
 
 freqnum=signum
-x=np.linspace(sigstart,sigstart+siglen,signum)
+x=np.linspace(sigstart,sigstart+siglen,signum+1)
 numx=np.linspace(0,freqnum-1,freqnum)
 #freqx=np.linspace(0,(freqnum-1)/siglen,freqnum)
 
 #信号函数
 def sig(x):
-    X=x*2*np.pi/siglen
+    #X=x*2*np.pi/siglen
     y=50*np.sin(5*x)+0.1*(10*x)**2+0.01*np.exp(0.001*x)*np.cos(x)+\
-        5*np.random.normal(3, 3, x.shape)
+        0#5*np.random.uniform(-15, 15, x.shape)*np.random.randint(0, 2, x.shape)
     #y=np.sin(X)
     #y=np.sin(X)+np.cos(2*X)
     #y=np.sin(x*2*np.pi/siglen)
@@ -44,7 +45,7 @@ def amp(freq,bsin=0):#frequency-amplitude    freq是波数
     
     s=sum(np.sin(x*2*np.pi*freq/siglen)*sig(x))
     c=sum(np.cos(x*2*np.pi*freq/siglen)*sig(x))
-    
+
     return [s,c]
 asin=[]
 acos=[]
@@ -55,35 +56,46 @@ for j in range(0,freqnum):
     
 
 #重建信号
-def sigr(cutfreq=1000,threshold=1000):
-    sigr=0
+def sigr(x,cutfreq=1000,threshold=0):#threshold=800
+    sig=0
     for j in range(1,freqnum):
         if(abs(asin[j])>=threshold):
-            sigr+=asin[j]*np.sin(2*np.pi*j*x/siglen)
+            sig+=asin[j]*np.sin(2*np.pi*j*x/siglen)
         if(abs(acos[j])>=threshold):   
-            sigr+=acos[j]*np.cos(2*np.pi*j*x/siglen)
+            sig+=acos[j]*np.cos(2*np.pi*j*x/siglen)
         if(j==cutfreq):
             break
-    return sigr
+    return sig
 
 asin=[round(x, 5) for x in asin]
 acos=[round(x, 5) for x in acos]
-plt.plot(numx,asin,label="sin")
 
-plt.grid()
-plt.legend()
-plt.show()
 
-plt.plot(numx,acos,label="cos")
-plt.grid()
-plt.legend()
-plt.show()
-plt.grid()
-plt.plot(x,sig(x))
-plt.grid()
-plt.show()
+def draw(x,y,label,hold=False):
+    plt.plot(x,y,label=label)
+    plt.grid()
+    plt.legend()
+    if(hold==0):
+        plt.show()
 
-plt.plot(x,sigr(threshold=0))
-plt.plot(x,sigr())
 
+draw(numx,asin,"sin",hold=True)
+draw(numx,acos,"cos")
+draw(x,(sigr(x=x)+20000)/30,"recover")
+draw(x,sig(x),"original")
+
+xx=np.linspace(sigstart,sigstart+siglen,signum*3)
+
+
+
+
+
+
+
+def test(m,n):#高频采样测试。为一个频率为m的函数执行n采样
+    z=np.linspace(0,n,n+1)
+    zz=np.sin(z*2*np.pi*m/n)
+    zz=[round(i, 7) for i in zz]
+    plt.plot(z,zz)
+# test(48,99)
 
